c9840e053e69d17d3c9a17417b766fd65824f30f
Gleb Son <57506521+glebs0n1@users.noreply.github.com>
2022-04-26 11:00:11 +0300

    Add files via upload

---
+++ src/com/company/Utils.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Utils {
+    private static final Scanner scanner = new Scanner(System.in);
+    public static Scanner getScanner(){
+        return scanner;
+    }
+}
